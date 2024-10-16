98d16ec62e79d0109748af049b0263e69b6c0826
kunaljunkie <109500415+kunaljunkie@users.noreply.github.com>
2024-10-16 10:40:14 +0530

    changes app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,36 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import patientRoutes from './routes/patientRoutes';
-import rateLimit from 'express-rate-limit'
+import express, { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import patientRoutes from "./routes/patientRoutes";
+import rateLimit from "express-rate-limit";
 import { config } from "dotenv";
 config();
 const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  message: 'Too many requests, please try again later.'
+  message: "Too many requests, please try again later.",
 });
 app.use(bodyParser.json());
-app.use(limiter)
+app.use(limiter);
 app.use(`/${process.env.ROUTES_PREFIX}`, patientRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/health',(req,res)=>{
-  res.status(200).json({message:"Server is up and running"})
-})
-
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is up and running" });
+});
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: "Internal Server Error",
+    error: err.message || "Something went wrong",
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
