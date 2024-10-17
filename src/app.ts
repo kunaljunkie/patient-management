5e453a71a5ec04b759a22d9312571ffe57f17f6c
kunaljunkie <kunalkumawat7860@gmail.com>
2024-10-17 12:06:19 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     error: err.message || "Something went wrong",
   });
 });
+console.log("kunal")
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
